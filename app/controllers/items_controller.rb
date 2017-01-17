2277fe1a0c0713a23ca1ebdd848020e873220266
Vi Phat Duong <phatdv@zigexn.vn>
2017-01-17 13:13:55 +0700

    Done Display Items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,11 @@ class ItemsController < ApplicationController
   def index
     # Fetch Newest Items
     page = params[:page] || 1
-    @items = Item.newest.page(page).per(10)
+    @items = Item.newest.page(page).per(4)
+  end
+
+  def show
+    @item = Item.find_by_id(params[:id])
   end
 
 
